f908535badd8cd1003481814aa206e522369b2dd
WaHotDog <www.957215439@qq.com>
2018-12-12 17:39:37 +0800

    去调无用的接口调用

--- src/main/java/com/device/management/ManageApplication.java
+++ src/main/java/com/device/management/ManageApplication.java
@@ -3,11 +3,10 @@ package com.device.management;
 import com.device.management.dataSource.DynamicDataSourceRegister;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 
 
 @SpringBootApplication
@@ -16,8 +15,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 })
 @MapperScan("com.device.management.mapper")
 @EnableScheduling
-
-public class ManageApplication {
+public class ManageApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ManageApplication.class, args);
